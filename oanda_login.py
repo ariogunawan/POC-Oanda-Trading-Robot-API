da2737551b8251d820a65013cb871e15443d80b4
Ario Gunawan <62988862+ariogunawan@users.noreply.github.com>
2024-09-02 14:01:27 +1000

    Update oanda_login.py

--- oanda_login.py
+++ oanda_login.py
@@ -5,8 +5,8 @@ Created on Sat May 23 15:41:43 2020
 @author: Ario Gunawan
 """
 login = {
-    'api_key' : 'bb0b5987ddb4ae39ff364ad45c7377bc-8dc59ab8f6656357f8de1db5929b54c3',
-    'account' : '101-011-13344081-001',
+    'api_key' : '',
+    'account' : '',
     'endpoint_url' : 'https://api-fxpractice.oanda.com'
 }
 
@@ -18,8 +18,8 @@ headers = {
 headers['Authorization'] = 'Bearer' + ' ' + login.get('api_key')
 
 mysql_login = {
-    'user' : 'root',
-    'password' : 'KucingjogetAbrakadabra123!',
-    'host' : '206.189.207.174',#'198.199.74.158',
+    'user' : '',
+    'password' : '',
+    'host' : '',#'198.199.74.158',
     'database' : 'oanda'
     }
